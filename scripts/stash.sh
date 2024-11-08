5c5efba04e6fcf7ba8bd35fb4b5320dd6a9286c7
RogerABoothe <Roger.A.Boothe@Gmail.com>
2024-11-08 08:00:36 -0600

    Fixed bug in stash line 8

--- scripts/stash.sh
+++ scripts/stash.sh
@@ -5,7 +5,7 @@ LOG=/home/$USER/scripts/backup.txt
 APP="stash"
 
 echo $(date "+%Y-%m-%d %H-%M-%S") "Process started for" $APP >> $LOG
-cp -r cp -r /home/arruser/$APP/config.yml /home/$USER/backup/$HOSTNAME/$APP &>> $LOG
+cp -r /home/arruser/$APP/config.yml /home/$USER/backup/$HOSTNAME/$APP &>> $LOG
 cp -r /home/$USER/$APP/stash-go.sqlite /home/$USER/backup/$HOSTNAME/$APP &>> $LOG
 cp -r /home/$USER/$APP/stash-go.sqlite-shm /home/$USER/backup/$HOSTNAME/$APP &>> $LOG
 cp -r /home/$USER/$APP/stash-go.sqlite-wal /home/$USER/backup/$HOSTNAME/$APP &>> $LOG
